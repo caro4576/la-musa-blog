6e80a5565c0ed57dde0e238997cf68728ebd90a4
caro <carojoamar@icloud.com>
2026-09-23 19:35:59 -0300

    fix: servir admin desde backend

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/admin", express.static(path.join(__dirname, "..", "admin")));
+app.use("/admin", express.static(path.join(__dirname, "admin")));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
